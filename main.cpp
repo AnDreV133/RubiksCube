fc147f7bca86b9bb57ebc04b2b70d172915f546d
AnDreV133 <dmitrijkubik@gmail.com>
2024-12-12 21:01:12 +0300

    fix: chang title

--- main.cpp
+++ main.cpp
@@ -402,7 +402,7 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(400, 350);
-    glutCreateWindow("Visualizacao 3D");
+    glutCreateWindow("Rubik's Cube 3D");
     glutDisplayFunc(draw_func);
     glutReshapeFunc(reshape_func);
 
